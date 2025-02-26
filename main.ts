5cbc169c963a432858c31f0837749a91938fc708
Jesper Jeeninga <23724638+moxventura@users.noreply.github.com>
2025-02-27 00:01:01 +0100

    Improve names

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
-function right () {
-    if (getValue(position_x + 1, position_y) == 0) {
+function moveRight () {
+    if (getValueFromMaze(position_x + 1, position_y) == 0) {
         basic.pause(200)
         position_x = position_x + 1
         drawMaze()
     }
 }
 function shuffle (array: number[][]) {
-    shuffled_array = []
+    let shuffled_array: number[][] = []
     for (let index = 0; index <= array.length - 1; index++) {
         shuffled_array.push(array[index])
     }
@@ -15,6 +15,13 @@ function shuffle (array: number[][]) {
         array.unshift(shuffled_array.removeAt(randint(0, shuffled_array.length - 1)))
     }
     return array
+}
+function moveUp () {
+    if (getValueFromMaze(position_x, position_y - 1) == 0) {
+        basic.pause(200)
+        position_y = position_y - 1
+        drawMaze()
+    }
 }
 function setSize () {
     height = randint(5, 7) * difficulty
@@ -26,19 +33,20 @@ function setSize () {
         width += 1
     }
 }
-function up () {
-    if (getValue(position_x, position_y - 1) == 0) {
-        basic.pause(200)
-        position_y = position_y - 1
-        drawMaze()
+function outputMazeToSerial () {
+    for (let i = 0; i <= width - 1; i++) {
+        mazeline = ""
+        for (let j = 0; j <= height - 1; j++) {
+            mazeline = "" + mazeline + maze[i][j]
+        }
+        serial.writeLine(mazeline)
     }
 }
-function left () {
-    if (getValue(position_x - 1, position_y) == 0) {
-        basic.pause(200)
-        position_x = position_x - 1
-        drawMaze()
+function getValueFromMaze (x: number, y: number) {
+    if (maze[x] && maze[x][y] != undefined) {
+        return maze[x][y]
     }
+    return 1
 }
 input.onButtonPressed(Button.A, function () {
     if (!(playing) && last_score) {
@@ -46,17 +54,22 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 function createMaze (w: number, h: number) {
-    maze = 0
-    new_maze = []
+    possible_directions = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1]
+    ]
+    maze = []
     for (let k = 0; k <= w - 1; k++) {
-        new_maze[k] = []
+        maze[k] = []
         for (let l = 0; l <= h - 1; l++) {
-            new_maze[k][l] = 1
+            maze[k][l] = 1
         }
     }
     // Initialize the stack with the starting position
     stack = [[0, 0]]
-    new_maze[0][0] = 0
+    maze[0][0] = 0
     while (stack.length > 0) {
         x = stack[stack.length - 1][0]
         y = stack[stack.length - 1][1]
@@ -68,28 +81,20 @@ function createMaze (w: number, h: number) {
             // Move two steps in the y direction
             chooseY = y + chosenDirection[1] * 2
             // Check if the new position is within bounds and is a wall
-            if (chooseX >= 0 && chooseX < new_maze[0].length && chooseY >= 0 && chooseY < new_maze.length && new_maze[chooseY][chooseX] == 1) {
-                new_maze[y + chosenDirection[1]][x + chosenDirection[0]] = 0
-                new_maze[chooseY][chooseX] = 0
+            if (chooseX >= 0 && chooseX < maze[0].length && chooseY >= 0 && chooseY < maze.length && maze[chooseY][chooseX] == 1) {
+                maze[y + chosenDirection[1]][x + chosenDirection[0]] = 0
+                maze[chooseY][chooseX] = 0
                 // Push the new cell onto the stack
                 stack.push([chooseX, chooseY])
             }
         }
     }
 }
-function getValue (x: number, y: number) {
-    if (new_maze[x] && new_maze[x][y] != undefined) {
-        return new_maze[x][y]
-    }
-    return 1
-}
-function output_maze () {
-    for (let i = 0; i <= width - 1; i++) {
-        mazeline = ""
-        for (let j = 0; j <= height - 1; j++) {
-            mazeline = "" + mazeline + new_maze[i][j]
-        }
-        serial.writeLine(mazeline)
+function moveDown () {
+    if (getValueFromMaze(position_x, position_y + 1) == 0) {
+        basic.pause(200)
+        position_y = position_y + 1
+        drawMaze()
     }
 }
 function gameOver () {
@@ -128,7 +133,7 @@ function drawMaze () {
         for (let n = 0; n <= 4; n++) {
             plotAtValue = 1
             if (m >= gridRow && n >= gridCol) {
-                plotAtValue = getValue(col + n - gridCol, row + m - gridRow)
+                plotAtValue = getValueFromMaze(col + n - gridCol, row + m - gridRow)
             }
             if (plotAtValue == undefined) {
                 plotAtValue = 1
@@ -150,27 +155,29 @@ input.onButtonPressed(Button.B, function () {
         basic.showNumber(difficulty)
     }
 })
-function down () {
-    if (getValue(position_x, position_y + 1) == 0) {
+function moveLeft () {
+    if (getValueFromMaze(position_x - 1, position_y) == 0) {
         basic.pause(200)
-        position_y = position_y + 1
+        position_x = position_x - 1
         drawMaze()
     }
 }
 function checkTilt () {
     xTilt = input.rotation(Rotation.Roll)
     yTilt = input.rotation(Rotation.Pitch)
-    if (xTilt < -10) {
-        left()
-    }
     if (xTilt > 10) {
-        right()
-    }
-    if (yTilt > 10) {
-        down()
+        moveRight()
+    } else {
+        if (xTilt < -10) {
+            moveLeft()
+        }
     }
     if (yTilt < -10) {
-        up()
+        moveUp()
+    } else {
+        if (yTilt > 10) {
+            moveDown()
+        }
     }
 }
 let yTilt = 0
@@ -182,32 +189,21 @@ let gridCol = 0
 let gridRow = 0
 let start_time = 0
 let end_time = 0
-let mazeline = ""
 let chooseY = 0
 let chooseX = 0
-let new_maze: number[][] = []
-let maze = 0
+let y = 0
+let x = 0
+let stack: number[][] = []
+let possible_directions: number[][] = []
 let last_score = 0
+let maze: number[][] = []
+let mazeline = ""
 let width = 0
 let height = 0
-let shuffled_array: number[][] = []
 let position_y = 0
 let position_x = 0
 let playing = false
 let difficulty = 0
-let stack: number[][] = []
-let y = 0
-let x = 0
-let possible_directions: number[][] = []
-possible_directions = [
-[1, 0],
-[0, 1],
-[-1, 0],
-[0, -1]
-]
-x = 0
-y = 0
-stack = []
 difficulty = 4
 playing = false
 basic.forever(function () {
