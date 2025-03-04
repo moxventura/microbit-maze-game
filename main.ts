df5d50177eed48636637bf025b13ce8d2a913b06
Jesper Jeeninga <23724638+moxventura@users.noreply.github.com>
2025-03-04 23:57:45 +0100

    Add comments and simplify

--- main.ts
+++ main.ts
@@ -1,28 +1,31 @@
-function moveRight () {
-    if (getValueFromMaze(position_x + 1, position_y) == 0) {
-        basic.pause(200)
-        position_x = position_x + 1
-        drawMaze()
-    }
-}
+/**
+ * A function to shuffle the given array using blockcode
+ * 
+ * First it will copy they array into a variabele shuffled_array
+ * Then it will empty the original array
+ * Finally it will randomly select one value from the suffled_array and move it to the new array
+ * 
+ */
 function shuffle (array: number[][]) {
+    // Create a copy of the array
     let shuffled_array: number[][] = []
     for (let index = 0; index <= array.length - 1; index++) {
         shuffled_array.push(array[index])
     }
+    // Empty original
     array = []
+    // While we still have items in our shuffled_array, pick a random one and add it to the array
     while (shuffled_array.length > 0) {
         array.unshift(shuffled_array.removeAt(randint(0, shuffled_array.length - 1)))
     }
     return array
 }
-function moveUp () {
-    if (getValueFromMaze(position_x, position_y - 1) == 0) {
-        basic.pause(200)
-        position_y = position_y - 1
-        drawMaze()
-    }
-}
+
+/**
+ * A function to create a semi-random maze size depending on difficulty
+ * It will check if the width or height is even and make it uneven
+ * An uneven value makes sure there are no walls against the boundaries
+ */
 function setSize () {
     height = randint(5, 7) * difficulty
     width = randint(5, 7) * difficulty
@@ -33,6 +36,11 @@ function setSize () {
         width += 1
     }
 }
+
+/**
+ * A function to draw the maze to the serial output.
+ * This is for debugging purposes and not actively used in the game
+ */
 function outputMazeToSerial () {
     for (let i = 0; i <= width - 1; i++) {
         mazeline = ""
@@ -42,17 +50,34 @@ function outputMazeToSerial () {
         serial.writeLine(mazeline)
     }
 }
+
+/**
+ * Provide an x and y to return the value of the maze on x,y
+ * If the value is out of bounds, it will return 1 (wall)
+ */
 function getValueFromMaze (x: number, y: number) {
     if (maze[x] && maze[x][y] != undefined) {
         return maze[x][y]
     }
     return 1
 }
+
+/**
+ * Show the last score of button.A press
+ */
 input.onButtonPressed(Button.A, function () {
     if (!(playing) && last_score) {
         basic.showString("" + (last_score))
     }
 })
+
+/**
+ * Function to create a maze
+ * It generates a maze using a randomized depth-first search algorithm. 
+ * It initializes a grid of specified width (w) and height (h) filled with walls (represented by 1). 
+ * Starting from the top-left corner, it explores random (shuffled) possible directions, carving paths (setting cells to 0) by moving two steps in a chosen direction and marking the intermediate cell as a path. 
+ * The stack is used to keep track of the current position in the maze and the cells that need to be explored next, allowing the algorithm to backtrack to previous positions when it reaches a dead end.
+ */
 function createMaze (w: number, h: number) {
     possible_directions = [
     [1, 0],
@@ -90,13 +115,11 @@ function createMaze (w: number, h: number) {
         }
     }
 }
-function moveDown () {
-    if (getValueFromMaze(position_x, position_y + 1) == 0) {
-        basic.pause(200)
-        position_y = position_y + 1
-        drawMaze()
-    }
-}
+
+/**
+ * Function when the game is over
+ * We play a victory sound, we'll stop playing and calculate a score based on time and difficulty
+ */
 function gameOver () {
     end_time = input.runningTime()
     music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Chase), music.PlaybackMode.InBackground)
@@ -107,6 +130,11 @@ function gameOver () {
     basic.clearScreen()
     basic.showString("" + (last_score))
 }
+
+/**
+ * A+B Starts the game.
+ * It resets the maze and puts the player on 0,0 (top left)
+ */
 input.onButtonPressed(Button.AB, function () {
     if (playing) {
         playing = false
@@ -124,20 +152,15 @@ input.onButtonPressed(Button.AB, function () {
         start_time = input.runningTime()
     }
 })
+
+/**
+ * This will draw the 5x5 part of the maze where the player is in the center of
+ * It will decide what coordinates to plot given the players position.
+ */
 function drawMaze () {
-    gridRow = Math.max(2 - position_y, 0)
-    gridCol = Math.max(2 - position_x, 0)
-    row = Math.max(position_y - 2, 0)
-    col = Math.max(position_x - 2, 0)
     for (let m = 0; m <= 4; m++) {
         for (let n = 0; n <= 4; n++) {
-            plotAtValue = 1
-            if (m >= gridRow && n >= gridCol) {
-                plotAtValue = getValueFromMaze(col + n - gridCol, row + m - gridRow)
-            }
-            if (plotAtValue == undefined) {
-                plotAtValue = 1
-            }
+            plotAtValue = getValueFromMaze(position_x - 2 + n, position_y - 2 + m)
             if (plotAtValue && plotAtValue != 0) {
                 led.plot(n, m)
             } else {
@@ -146,6 +169,9 @@ function drawMaze () {
         }
     }
 }
+/**
+ * Button B is used to change the difficulty between 1 and 9
+ */
 input.onButtonPressed(Button.B, function () {
     if (!(playing)) {
         difficulty += 1
@@ -155,6 +181,11 @@ input.onButtonPressed(Button.B, function () {
         basic.showNumber(difficulty)
     }
 })
+
+/**
+ * Move function, pause and change the players position if the maze has a path (0) on the given position
+ * And redraw the maze
+ */
 function moveLeft () {
     if (getValueFromMaze(position_x - 1, position_y) == 0) {
         basic.pause(200)
@@ -162,6 +193,45 @@ function moveLeft () {
         drawMaze()
     }
 }
+/**
+ * Move function, pause and change the players position if the maze has a path (0) on the given position
+ * And redraw the maze
+ */
+function moveRight() {
+    if (getValueFromMaze(position_x + 1, position_y) == 0) {
+        basic.pause(200)
+        position_x = position_x + 1
+        drawMaze()
+    }
+}
+/**
+ * Move function, pause and change the players position if the maze has a path (0) on the given position
+ * And redraw the maze
+ */
+function moveUp() {
+    if (getValueFromMaze(position_x, position_y - 1) == 0) {
+        basic.pause(200)
+        position_y = position_y - 1
+        drawMaze()
+    }
+}
+/**
+ * Move function, pause and change the players position if the maze has a path (0) on the given position
+ * And redraw the maze
+ */
+function moveDown() {
+    if (getValueFromMaze(position_x, position_y + 1) == 0) {
+        basic.pause(200)
+        position_y = position_y + 1
+        drawMaze()
+    }
+}
+
+/**
+ * This function will check the tilt (rotation) of the microbit
+ * Whenever we tilt it more then 10 degrees to one side, we'll change the player's position
+ * This allows steering the player using the fitbit's tilt.
+ */
 function checkTilt () {
     xTilt = input.rotation(Rotation.Roll)
     yTilt = input.rotation(Rotation.Pitch)
@@ -183,10 +253,6 @@ function checkTilt () {
 let yTilt = 0
 let xTilt = 0
 let plotAtValue = 0
-let col = 0
-let row = 0
-let gridCol = 0
-let gridRow = 0
 let start_time = 0
 let end_time = 0
 let chooseY = 0
@@ -206,6 +272,10 @@ let playing = false
 let difficulty = 0
 difficulty = 4
 playing = false
+/**
+ * Toggeling the center led each gameloop will allow it to 'flicker' making a distinction with the walls.
+ * Whenever the position reaches the bottom right of the maze, we end the game.
+ */
 basic.forever(function () {
     if (playing) {
         led.toggle(2, 2)
